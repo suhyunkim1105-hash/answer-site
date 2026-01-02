a1e2a8266517833872bcb80631c94c7aef873176
suhyunkim1105-hash <suhyunkim1105@gmail.com>
2026-01-02 20:27:12 +0900

    Delete config.js

--- netlify/functions/config.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/config.js
-export const handler = async () => {
-  // STOP_TOKEN은 공개되어도 괜찮은 값(문제지에 없을 임의 문자열)
-  const stopToken = process.env.STOP_TOKEN || "ABCDEFGH";
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ stopToken }),
-  };
-};
